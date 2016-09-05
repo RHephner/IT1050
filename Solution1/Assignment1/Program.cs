8195897d94993f4750be689cf0ca443f16625c7f
RHephner <rhep10@gmail.com>
2016-09-05 18:42:51 -0400

    New Project

--- Solution1/Assignment1/Program.cs
+++ Solution1/Assignment1/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestApplication
+namespace Assignment1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World");
         }
     }
 }
