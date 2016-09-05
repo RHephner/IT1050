7aad3a257706ff522d9134e7574b78ff5b822ed8
RHephner <rhep10@gmail.com>
2016-09-05 19:21:44 -0400

    Assignment1

---
+++ Solution1/Section1/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Section1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string middleInitial;
+            string lastName;
+            string fullName;
+            int age;
+            bool isCitizen;
+            bool canVote;
+            int heightFeet;
+            double heightInches;
+            double totalHeightCM;
+
+
+            System.Console.Write("What is your first name?");
+            firstName = System.Console.ReadLine();
+
+            System.Console.Write("What is your middle initial?");
+            middleInitial = System.Console.ReadLine();
+
+            System.Console.Write("What is your last name?");
+            lastName = System.Console.ReadLine();
+
+
+            fullName = firstName + " " + middleInitial + ". " + lastName;
+
+            System.Console.Write("What is height in feet? (4, 5, 6 etc...)");
+            heightFeet = int.Parse(System.Console.ReadLine());
+
+            System.Console.Write("How many inches are you beyond your base height in feet?");
+            heightInches = double.Parse(System.Console.ReadLine());
+
+            totalHeightCM = ((heightFeet * 12) + heightInches) * 2.54;
+
+            System.Console.Write("What is your age?");
+            age = int.Parse(System.Console.ReadLine());
+
+            System.Console.Write("True or false: are you a citizen of this country?");
+            isCitizen = bool.Parse(System.Console.ReadLine());
+
+            canVote = age >= 18 && isCitizen;
+
+            System.Console.WriteLine("Hello, " + fullName);
+            System.Console.WriteLine("Your Height is: " + totalHeightCM);
+            System.Console.WriteLine("It is " + canVote + " that you are eligible to vote");
+            System.Console.ReadKey();
+
+        }
+    }
+}
--- Solution1/Section1/Properties/AssemblyInfo.cs
+++ Solution1/Section1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Assignment1")]
+[assembly: AssemblyTitle("Section1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Assignment1")]
+[assembly: AssemblyProduct("Section1")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a1ddcbf4-ce9b-4e09-ada6-2bf3e298b50e")]
+[assembly: Guid("f84888b5-b494-43c9-9aa3-e1c9ac6e5f5d")]
 
 // Version information for an assembly consists of the following four values:
 //
