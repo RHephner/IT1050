3d3d238985637a207f8864261d42f87e8ae2c7cc
RHephner <rhep10@gmail.com>
2016-09-27 19:51:01 -0400

    Add files via upload

---
+++ Solution2/Section2/Person.cs
@@ -0,0 +1,33 @@
+﻿namespace Section2
+{
+    public class Person
+    {
+        public int Age;
+        public string FirstName;
+        public string LastName;
+        public Person Spouse;
+
+        public string GetFullName()
+        {
+        string fullName = FirstName + " " + LastName;
+        return fullName;
+
+        }
+        
+        public static double SumOfAllAges
+        {
+            //sum of all ages divided by number of people
+            //Person.SumOfAllAges +=p1.Age
+            //Github work already..
+
+
+        }
+        
+        public void PrintNameAndAge
+        {
+       
+
+
+        }
+    }
+}
---
+++ Solution2/Section2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Section2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Spouse = new Person();
+            //Update This
+            //blah
+
+        }
+    }
+}
