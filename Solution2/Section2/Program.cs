6f0d1d567791e6e1941aa800db6083861b2ee305
RHephner <rhep10@gmail.com>
2016-10-04 00:24:49 -0400

    It works

--- Solution2/Section2/Program.cs
+++ Solution2/Section2/Program.cs
@@ -6,12 +6,37 @@
         {
             Person p1 = new Person();
             p1.Spouse = new Person();
-            //Update This
-            //blah
+
+
             Person p2 = new Person();
             p2.Spouse = new Person();
 
-            //System.Console.wri
+            // System.Console.Write("");
+            //p1.FirstName = System.Console.ReadLine();
+
+            System.Console.WriteLine("[ Enter Information for p1 ]");
+            System.Console.WriteLine("");
+
+            p1.PersonInfo();
+            p1.SpouseInfo();
+
+            System.Console.WriteLine("");
+            System.Console.WriteLine("[ Enter Information for p2 ]");
+            System.Console.WriteLine("");
+
+            p2.PersonInfo();
+            p2.SpouseInfo();
+
+            System.Console.WriteLine("");
+            System.Console.WriteLine("[ Printing Reults ]");
+            System.Console.WriteLine("");
+
+            p1.PrintNameAndAge();
+            p2.PrintNameAndAge();
+
+            System.Console.WriteLine("");
+            System.Console.WriteLine("Average Age = " + Person.AverageAge());
+            System.Console.Read();
         }
     }
 }
